5679333addb8165ce66177979076c5f6bb314c95
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-27 22:48:54 +0100

    app ading multiple options and drawing one of them

--- main.js
+++ main.js
@@ -1,50 +1,27 @@
-const choice = ['dad', 'lal'];
-const addBtn = document.querySelector('.add');
-const resetBtn = document.querySelector('.clean');
-const adviceBtn = document.querySelector('.showAdvice');
-const optionsBtn = document.querySelector('.showOptions');
-const advice = document.querySelector('h1');
+const choices = [];
+const btnAdd = document.querySelector('.add');
+const btnReset = document.querySelector('.clean');
+const btnShow = document.querySelector('.showAdvice');
+const btnShowAll = document.querySelector('.showOptions');
+const text = document.querySelector('h1');
 
-// dodawanie 
-addBtn.addEventListener('click', (e) => {
+//-----adding----- 
+btnAdd.addEventListener('click', (e) => {
     e.preventDefault();
-    let input = document.querySelector('input');
-    if (input.value.length) {
-        for (any of choice) {
-            if (any === input.value) {
-                window.alert(`juz takie mamy`);
-                return;
+    const input = document.querySelector('input');
+    if (input.value) {
+        for (one of choices) {
+            if (one === input.value) {
+                alert('already added')
+                return
             }
         }
-        choice.push(input.value);
-        alert(`dodales ${input.value}`)
+        choices.push(input.value);
+        alert(`${input.value} added`);
         input.value = '';
-        console.log(choice);
     } else {
-        window.alert(`uzupelnij pole`)
+        alert('can not add a empty string')
     }
 })
 
-//reset 
-resetBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    choice.length = 0;
-    // choice = [];
-    advice.textContent = '';
-})
-
-//porada 
-adviceBtn.addEventListener('click', () => {
-    const choiceIndex = Math.floor(Math.random() * choice.length);
-    advice.textContent = choice[choiceIndex];
-})
-
-//mozliwosci 
-optionsBtn.addEventListener('click', () => {
-    alert(choice.join('---'));
-    // let options = 'mozliwe opcje to: --- ';
-    // for (i = 0; i < choice.length; i++) {
-    //     options += choice[i] + ' --- ';
-    // }
-    // window.alert(options);
-})
+//----reset-----
